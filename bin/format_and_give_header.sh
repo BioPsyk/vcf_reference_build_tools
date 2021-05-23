fb7defc1743226ee9dfe53dc29051cbd56ea3095
pappewaio <jesper.gadin@gmail.com>
2021-05-23 21:34:26 +0200

    addded the full formats for respective original format and the lifted over format

--- bin/format_and_give_header.sh
+++ bin/format_and_give_header.sh
@@ -6,9 +6,9 @@ build=$3
 
 # Add header and cut off rownindex
 if [ "${build}" == "GRCh38" ]; then
-  echo -e "ROWINDEX CHR POS ID REF ALT CHROM_GRCh37 POS_GRCh37 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151" > ${outfile}
+  echo -e "ROWINDEX CHR POS ID REF ALT CHROM_GRCh37 POS_GRCh37 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151 SNP_full SNP_full_GRCh37" > ${outfile}
 else
-  echo -e "ROWINDEX CHR POS ID REF ALT CHROM_GRCh38 POS_GRCh38 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151" > ${outfile}
+  echo -e "ROWINDEX CHR POS ID REF ALT CHROM_GRCh38 POS_GRCh38 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151 SNP_full SNP_full_GRCh38" > ${outfile}
 fi
 
 awk '
@@ -18,9 +18,11 @@ awk '
   if($6=="NA"){pos3="NA"; pos4="NA"
   }else{split($6,gr,":"); pos3=gr[1]; pos4=gr[2]};
 
- # if($7=="NA"){full=$3"_"$4"_"$6"_"$7}else{full=$7};
- # if($7=="NA"){full2=$3"_"$4"_"$6"_"$7}else{full2=$7};
+  # if not in dbsnp use orignal chr_pos_ref_alt marker
+  if($7=="NA"){full=pos1"_"pos2"_"$4"_"$5}else{full=$7};
+  # if not in dbsnp use . (as no hit in dbsnp means no chr:pos information)
+  if($7=="NA"){full2="."}else{full2=$7};
 
-  print $1, pos1, pos2, $3, $4, $5, pos3, pos4, $7, $8, $9
+  print $1, pos1, pos2, $3, $4, $5, pos3, pos4, $7, $8, $9, full, full2
 }' ${infile} >> ${outfile}
 
--- tests/unit/test_format_and_give_header.sh
+++ tests/unit/test_format_and_give_header.sh
@@ -50,31 +50,19 @@ echo ">> Test ${test_script}"
 #---------------------------------------------------------------------------------
 # Check that the final output is what we think it is
 
-_setup "Format final output"
+_setup "missing dbsnp hit"
 
 cat <<EOF > ./input.tsv
-8 NA . G A 10:106524737 NA NA NA
-9 NA . T C 10:101966771 NA NA NA
-10 10:104573936 . T C 10:104573936 rs284858 T C
-11 NA . T C 10:104355789 NA NA NA
+10 10:104573936 . T C NA NA NA NA
 12 10:10616485 . T C 10:10616485 rs2025468 T C
-13 NA . A G 10:105905360 NA NA NA
-14 NA . C T 10:106322887 NA NA NA
 15 10:108131461 . C A 10:108131461 rs1409409 C A
-16 NA . C T 10:100157763 NA NA NA
 EOF
 
 cat <<EOF > ./expected-result1.tsv
-ROWINDEX CHR POS ID REF ALT CHROM_GRCh38 POS_GRCh38 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151
-8 NA NA . G A 10 106524737 NA NA NA
-9 NA NA . T C 10 101966771 NA NA NA
-10 10 104573936 . T C 10 104573936 rs284858 T C
-11 NA NA . T C 10 104355789 NA NA NA
-12 10 10616485 . T C 10 10616485 rs2025468 T C
-13 NA NA . A G 10 105905360 NA NA NA
-14 NA NA . C T 10 106322887 NA NA NA
-15 10 108131461 . C A 10 108131461 rs1409409 C A
-16 NA NA . C T 10 100157763 NA NA NA
+ROWINDEX CHR POS ID REF ALT CHROM_GRCh38 POS_GRCh38 ID_dbSNP151 REF_dbSNP151 ALT_dbSNP151 SNP_full SNP_full_GRCh38
+10 10 104573936 . T C NA NA NA NA NA 10_104573936_T_C .
+12 10 10616485 . T C 10 10616485 rs2025468 T C rs2025468 rs2025468
+15 10 108131461 . C A 10 108131461 rs1409409 C A rs1409409 rs1409409
 EOF
 
 _run_script
@@ -82,16 +70,4 @@ _run_script
 #---------------------------------------------------------------------------------
 # Next case
 
-#_setup "valid_rows_missing_afreq"
-#
-#cat <<EOF > ./acor.tsv
-#0	A1	A2	CHRPOS	RSID	EffectAllele	OtherAllele	EMOD
-#1	A	G	12:126406434	rs1000000	G	A	-1
-#EOF
-#
-#cat <<EOF > ./stat.tsv
-#0	B	SE	Z	P
-#1	-0.0143	0.0156	-0.916667	0.3604
-#EOF
-#
-#_run_script
+
