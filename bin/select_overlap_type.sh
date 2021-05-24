f470bf80c60ba9564f3db2fb17ba6de77f52a1f1
pappewaio <jesper.gadin@gmail.com>
2021-05-24 21:56:47 +0200

    select overlap type function ready, but untestet

---
+++ bin/select_overlap_type.sh
@@ -0,0 +1,32 @@
+infile=$1
+oltype=$2
+
+# Count NA or . in respective column
+if [ "$oltype" == "Both_RSID_exists"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if($4 != "." && $9 != "NA"){print $0}; 
+  }'
+elif [ "$oltype" == "only_dbSNP_RSID_exists"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if($4 == "." && $9 != "NA"){print $0}; 
+  }'
+elif [ "$oltype" == "only_input_RSID_exists"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if($4 != "." && $9 == "NA"){print $0}; 
+  }'
+elif [ "$oltype" == "No_RSID_exists"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if($4 == "." && $9 == "NA"){print $0}
+  }'
+elif [ "$oltype" == "Both_RSID_exists_and_same"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if(($4 != "." && $9 != "NA") && ($4 == $9)){print $0}
+  }'
+elif [ "$oltype" == "Both_RSID_exists_not_same"  ]; then
+gunzip -c $infile | awk ' NR>1{
+  if(($4 != "." && $9 != "NA") && ($4 != $9)){print $0}
+  }'
+else
+  echo "oltype doesnt exist"
+fi
+
